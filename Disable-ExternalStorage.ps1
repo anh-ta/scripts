a6afbe4c075c94fbaa1bcea7b7e1ed8fa07e4324
maqanht <30351444+maqanht@users.noreply.github.com>
2023-10-10 09:17:16 -0700

    Update Disable-ExternalStorage.ps1

--- Disable-ExternalStorage.ps1
+++ Disable-ExternalStorage.ps1
@@ -1,13 +1,1 @@
-$RemovableStorageDevicesKey = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices'
-if (-not (Test-Path $RemovableStorageDevicesKey)) {
-    New-Item -Path $RemovableStorageDevicesKey -Force | Out-Null
-}
-Set-ItemProperty -Path $RemovableStorageDevicesKey -Name "Deny_All" -Value 1 -Type DWORD -Force | Out-Null
 
-
-
-$UsbStorKey = 'HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR'
-if (-not (Test-Path $UsbStorKey)) {
-    New-Item -Path $UsbStorKey -Force | Out-Null
-}
-Set-ItemProperty -Path $UsbStorKey -Name "Start" -Value 4 -Type DWORD -Force | Out-Null
